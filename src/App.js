a4a645f61523f63a7a8ad745f9f5d407453e7336
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-02-28 10:38:13 +0100

    add basic functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,84 @@
+import React, { useRef, useState, useEffect } from 'react';
+import Header from './components/Header';
+import CustomCircularProgressbar from './components/CustomCircularProgressbar';
+import MainButton from './components/MainButton';
+
+const focusTime = 10000;
+const shortBrakeTime = 5000;
+const longBrakeTime = 8000;
+
 function App() {
+  const timerRef = useRef();
+  const [time, setTime] = useState({
+    total: focusTime,
+    remaining: focusTime,
+  });
+  const [step, setStep] = useState(1);
+  const [mode, setMode] = useState('focus');
+  const [isPaused, setIsPaused] = useState(true);
+
+  const preserveRemainingTime = () => {
+    const timerApi = timerRef.current;
+    setTime({ ...time, remaining: timerApi.state.timeDelta.total });
+  };
+
+  const handleClick = () => {
+    if (isPaused) {
+      preserveRemainingTime();
+      setIsPaused(false);
+    } else {
+      setIsPaused(true);
+    }
+  };
+
+  const handleCountdownCopmlete = () => {
+    setTimeout(() => {
+      setStep(step + 1);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    const timerApi = timerRef.current;
+    if (isPaused) {
+      timerApi.pause();
+    } else {
+      timerApi.start();
+    }
+  }, [isPaused, step]);
+
+  useEffect(() => {
+    if (step % 2 === 0) {
+      if (step % 8 === 0) {
+        setMode('long brake');
+      } else {
+        setMode('short brake');
+      }
+    } else {
+      setMode('focus');
+    }
+  }, [step]);
+
+  useEffect(() => {
+    if (mode === 'focus') {
+      setTime({ total: focusTime, remaining: focusTime });
+    } else if (mode === 'short brake') {
+      setTime({ total: shortBrakeTime, remaining: shortBrakeTime });
+    } else if (mode === 'long brake') {
+      setTime({ total: longBrakeTime, remaining: longBrakeTime });
+    }
+  }, [mode]);
+
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Header />
+      <CustomCircularProgressbar
+        remainingTime={time.remaining}
+        totalTime={time.total}
+        onComplete={handleCountdownCopmlete}
+        coundownRef={timerRef}
+      />
+      <span className="app__info">{mode}</span>
+      <MainButton onClick={handleClick} icon={isPaused ? 'run' : 'pause'} />
     </div>
   );
 }
--- src/components/CustomCircularProgressbar.jsx
+++ src/components/CustomCircularProgressbar.jsx
@@ -1,19 +1,39 @@
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import Countdown from 'react-countdown';
 
-const CustomCircularProgressbar = () => {
-  const value = 0.66;
-  return (
+const CustomCircularProgressbar = ({
+  remainingTime,
+  totalTime,
+  onComplete,
+  coundownRef,
+}) => {
+  const renderer = ({ minutes, seconds, milliseconds }) => (
     <div className="custom-circular-progressbar">
       <div className="custom-circular-progressbar__wrapper">
         <CircularProgressbarWithChildren
-          value={value}
-          maxValue={1}
+          value={milliseconds + seconds * 1000 + minutes * 60000}
+          maxValue={totalTime}
         >
-          <span className="custom-circular-progressbar__time">20</span>
-          <span className="custom-circular-progressbar__info">minutes</span>
+          <span className="custom-circular-progressbar__time">
+            {minutes < 1 ? seconds : minutes}
+          </span>
+          <span className="custom-circular-progressbar__info">
+            {minutes < 1 ? 'seconds' : 'minutes'}
+          </span>
         </CircularProgressbarWithChildren>
       </div>
     </div>
+  );
+  return (
+    <Countdown
+      date={Date.now() + remainingTime}
+      autoStart={false}
+      intervalDelay={100}
+      precision={3}
+      onComplete={onComplete}
+      renderer={renderer}
+      ref={coundownRef}
+    />
   );
 };
 
