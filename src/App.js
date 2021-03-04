cb15fed5e3e79610e10c1a21a60e9d10b80afe39
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2021-03-04 08:53:07 +0100

    lift up state from range item to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Timer from './components/Timer';
+import Menu from './components/Menu';
 
 function App() {
+  const [focusValue, setFocusValue] = useState([1]);
+  const [shortBrakeValue, setShortBrakeValue] = useState([1]);
+  const [longBrakeValue, setLongBrakeValue] = useState([1]);
+
   return (
     <div className="app">
       <Header />
-      <Timer />
+      
+      <Menu
+        focusValue={focusValue}
+        setFocusValue={setFocusValue}
+        shortBrakeValue={shortBrakeValue}
+        setShortBrakeValue={setShortBrakeValue}
+        longBrakeValue={longBrakeValue}
+        setLongBrakeValue={setLongBrakeValue}
+      />
     </div>
   );
 }
